0553757d4b93fe4758f6a7793e88fceba766e2af
Lucas <lugano1995@gmail.com>
2019-08-21 18:47:13 -0300

    Created server, users and authentication JWT

---
+++ src/database/index.js
@@ -0,0 +1,18 @@
+import Sequelize from 'sequelize';
+import User from '../app/models/UserModel';
+import databaseConfig from '../config/database';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.connection = new Sequelize(databaseConfig);
+    this.init();
+  }
+
+  init() {
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
